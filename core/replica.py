4f318a55780a360b87079e412b7336c929ef6e66
Jesus Cantor <jesus.cantor@wiedii.co>
2023-12-03 17:44:23 -0500

    Connect to nodos finish method

--- core/replica.py
+++ core/replica.py
@@ -1,31 +1,37 @@
-#Esto tan solo es un ejemplo de como se puede conectar a una base de datos de mongoDB.
-
-
 import pymongo
 
 # Conectarse al nodo maestro
-client = pymongo.MongoClient("mongodb://localhost:27017")
+client = pymongo.MongoClient("mongodb://localhost:27017/replicaSet=rs0")
 
-# Configurar la replicación
-config = {
-    "_id": "eventos_deportivos",
-    "members": [
-        {"_id": 0, "host": "localhost:27017"},
-        {"_id": 1, "host": "localhost:27018"},
-        {"_id": 2, "host": "localhost:27019"}
-    ]
-}
+Db = 'torneo_deportivo'
+testColl = 'testColl'
 
-try:
-    client.admin.command("replSetInitiate", config)
-except pymongo.errors.OperationFailure as e:
-    print(f"Error al iniciar el conjunto de réplicas: {e}")
+# Insertar un registro
+doc = dict(DEMO="Python for demo", MESG="Hello ApsaraDB For MongoDB")
+doc_id = client[Db][testColl].insert_one(doc).inserted_id
 
-# Conectarse al nodo maestro
-client = pymongo.MongoClient("mongodb://localhost:27017")
+# Consultar el registro
+for d in client[Db][testColl].find(dict(DEMO="Python for demo")):
+    print ('find documents: DB1', d)
 
-# Obtener el estado de la replicación
-status = client.admin.command("replSetGetStatus")
+client.close()
 
-# Imprimir el estado de la replicación
-print(status)
+# Repetimos el proceso en el segundo nodo
+client2 = pymongo.MongoClient("mongodb://localhost:27018/replicaSet=rs0")
+Db = 'torneo_deportivo'
+testColl = 'testColl'
+doc = dict(DEMO="Python for demo", MESG="Hello ApsaraDB For MongoDB")
+doc_id = client2[Db][testColl].insert_one(doc).inserted_id
+for d in client2[Db][testColl].find(dict(DEMO="Python for demo")):
+    print ('find documents: DB2', d)
+client2.close()
+
+# Repetimos el proceso en el tercer nodo
+client3 = pymongo.MongoClient("mongodb://localhost:27019/replicaSet=rs0")
+Db = 'torneo_deportivo'
+testColl = 'testColl'
+doc = dict(DEMO="Python for demo", MESG="Hello ApsaraDB For MongoDB")
+doc_id = client3[Db][testColl].insert_one(doc).inserted_id
+for d in client3[Db][testColl].find(dict(DEMO="Python for demo")):
+    print ('find documents DB3:', d)
+client3.close()
